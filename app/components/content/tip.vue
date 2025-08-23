b2de551307892030b984acf4637f3e108d3d7d80
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-08-23 19:49:36 +0800

    feat: 添加tip组件

---
+++ app/components/content/tip.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="mb-5 py-3 px-4 border-1 border-orange-300 rounded-xl bg-orange-50">
+        <p class="font-bold mb-2!">TIP</p>
+        <slot mdc-unwrap="p" />
+    </div>
+</template>
