e989a5207d9caa22838dc38848dd3e424f3b231d
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-08-23 20:10:34 +0800

    feat: 添加提示组件

---
+++ app/components/content/danger.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="mb-5 py-3 px-4 border-1 border-red-300 rounded-xl bg-red-50">
+        <p class="font-bold mb-2!">DANNGER</p>
+        <slot mdc-unwrap="p" />
+    </div>
+</template>
