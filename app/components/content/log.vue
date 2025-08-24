5105f404ca6e529392d154115b472be6b9f72ca6
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-08-24 13:39:01 +0800

    feat: 完善服务器日志页

---
+++ app/components/content/log.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="h-screen">
+		<div class="fixed top-0 -z-1 h-full w-0.5 bg-gray-100"></div>
+		<div v-for="item in [...log].reverse()" :key="item.time" class="relative">
+			<div class="time ml-6 flex flex-col justify-center gap-3">
+				<div class="text-gray-500">{{ item.time }}</div>
+				<div class="flex-1">{{ item.content }}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+interface log {
+	time: string;
+	content: string;
+}
+
+const log = ref<log[]>([
+	{
+		time: "2025/8/8",
+		content: "开启测试服",
+	},
+	{
+		time: "2025/8/17",
+		content: "开启正式服",
+	},
+	{
+		time: "2025/8/24",
+		content: "游戏版本从 1.21.6 升级到 1.21.7",
+	},
+]);
+</script>
+
+<style lang="scss" scoped>
+.time::before {
+	content: "";
+	width: 0.5rem;
+	height: 0.5rem;
+	border-radius: 9999px;
+	background-color: oklch(83.7% 0.128 66.29);
+	position: relative;
+    left: -1.7rem;
+    top: 1.8rem;
+}
+</style>
--- content/docs/log.md
+++ content/docs/log.md
@@ -4,3 +4,6 @@ icon: gravity-ui:file
 index: 8
 title: 服务器日志
 ---
+
+::log
+::
