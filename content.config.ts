3a5987f9211205674a40bd557dc7c3438fd061c7
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-08-25 15:28:25 +0800

    fix: 修复文章无法热重载的问题

--- content.config.ts
+++ content.config.ts
@@ -2,6 +2,16 @@ import { defineCollection, defineContentConfig, z } from "@nuxt/content";
 
 export default defineContentConfig({
 	collections: {
+		docs: defineCollection({
+			type: "page",
+			source: "docs/**/*.md",
+			schema: z.object({
+				title: z.string(),
+				icon: z.string(),
+				cover: z.string(),
+				isDir: z.boolean(),
+			}),
+		}),
 		main_list: defineCollection({
 			type: "page",
 			source: "docs/*.md",
@@ -27,25 +37,6 @@ export default defineContentConfig({
 				icon: z.string(),
 				index: z.string(),
 			})
-		}),
-		// playing_list: defineCollection({
-		//     type: "page",
-		// 	source: "docs/playing/*.md",
-		// 	schema: z.object({
-		// 		title: z.string(),
-		// 		icon: z.string(),
-		// 		index: z.number(),
-		// 	})
-		// }),
-		docs: defineCollection({
-			type: "page",
-			source: "docs/**/*.md",
-			schema: z.object({
-				title: z.string(),
-				icon: z.string(),
-				cover: z.string(),
-				isDir: z.boolean(),
-			}),
 		}),
 	},
 });
