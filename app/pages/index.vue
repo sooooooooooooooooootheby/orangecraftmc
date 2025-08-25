40313ab6cdac2389603b55b32e9e0a7ba67cdf8a
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-08-25 16:34:58 +0800

    feat: 修改首页的样式

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="w-screen h-screen flex flex-col justify-center items-center gap-2">
-        <img src="/orange.png" alt="logo" class="w-32 mb-6">
+        <img src="/orange.png" alt="logo" class="w-32 mb-6 animate-spin" style="animation-duration: 12s;">
         <p class="text-4xl font-bold">~ Orange Craft MC ~</p>
+        <div class="w-full max-w-4xl border-t-1 mb-2"></div>
         <NuxtLink to="/docs" class="text-lg hover:underline">docs</NuxtLink>
     </div>
 </template>
