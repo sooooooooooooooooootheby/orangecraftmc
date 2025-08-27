43eead3c86171d132f91bb079b464ff58871b842
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-08-27 12:10:01 +0800

    fix: 修复目录变量存放问题导致页面刷新之后内容丢失的问题

--- app/pages/docs/[...slug].vue
+++ app/pages/docs/[...slug].vue
@@ -1,22 +1,22 @@
 <template>
 	<div v-if="docs" class="flex flex-col items-center">
-		<div class="flex h-[320px] w-full items-center overflow-hidden" v-if="docs.cover">
-			<img :src="docs.cover" alt="cover" class="h-full w-full object-cover" />
+		<div class="flex h-[320px] w-full items-center overflow-hidden" v-if="docs.d?.cover">
+			<img :src="docs.d?.cover" alt="cover" class="h-full w-full object-cover" />
 		</div>
 		<div class="pt-8">
 			<div class="mx-auto w-full max-w-[1200px] px-4 md:px-6">
-				<div class="grid grid-cols-1 items-start gap-6 md:grid-cols-[minmax(0,1fr)_16rem]" :class="{ 'flex!': !docs.isDir }">
-					<main class="w-full min-w-0" :class="{ 'max-w-[1200px]': !docs.isDir, 'max-w-[65ch]': docs.isDir }">
+				<div class="grid grid-cols-1 items-start gap-6 md:grid-cols-[minmax(0,1fr)_16rem]" :class="{ 'flex!': !docs.d?.isDir }">
+					<main class="w-full min-w-0" :class="{ 'max-w-[1200px]': !docs.d?.isDir, 'max-w-[65ch]': docs.d?.isDir }">
 						<div class="typography mx-auto">
-							<ContentRenderer :value="docs" />
+							<ContentRenderer :value="docs.d ?? {}" />
 						</div>
 					</main>
 
-					<aside v-if="docs.isDir" class="hidden w-64 flex-shrink-0 self-start md:block">
+					<aside v-if="docs.d?.isDir" class="hidden w-64 flex-shrink-0 self-start md:block">
 						<div class="sticky top-4 max-h-[calc(100vh-1rem)] overflow-auto">
 							<p class="mb-2 text-lg font-bold">On This Page</p>
 							<ul>
-								<li v-for="item in dir" :key="item.id" class="text-sm text-gray-700">
+								<li v-for="item in docs.dir" :key="item.id" class="text-sm text-gray-700">
 									<a :href="'#' + item.id" class="block py-1 hover:text-orange-400">
 										{{ item.text }}
 									</a>
@@ -41,12 +41,12 @@ interface dir {
 	text: string;
 }
 
-const dir = ref<dir[]>([]);
+// const dir = ref<dir[]>([]);
 
 const route = useRoute();
 const { data: docs } = await useAsyncData(route.path, async () => {
 	const d = await queryCollection("docs").path(route.path).first();
-	dir.value =
+	const dir =
 		d?.body.value
 			?.filter((item: any) => typeof item[0] === "string" && item[0].startsWith("h"))
 			.map((item: any) => ({
@@ -55,11 +55,11 @@ const { data: docs } = await useAsyncData(route.path, async () => {
 				text: item[2],
 			})) || [];
 
-	return d;
+	return { d, dir };
 });
 
 useSeoMeta({
-	title: `${docs.value?.title} | Orange Craft Mc`,
-	ogTitle: `${docs.value?.title} | Orange Craft Mc`,
+	title: `${docs.value?.d?.title} | Orange Craft Mc`,
+	ogTitle: `${docs.value?.d?.title} | Orange Craft Mc`,
 });
 </script>
